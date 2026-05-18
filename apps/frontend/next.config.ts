79c2ed428df5c7a23bb7c1b9ad5bf6e7ab0ffa2c
Mohd Hussain <m.sunesara@somaiya.edu>
2026-05-18 11:39:37 +0530

    fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,9 +1,15 @@
+// MODULES //
+import path from "path";
+
 // OTHERS //
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
