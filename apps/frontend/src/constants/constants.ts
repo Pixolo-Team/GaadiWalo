4327b96d5b667ce45d91ecb2dbdeb35bd4d9b5df
Mohd Hussain <m.sunesara@somaiya.edu>
2026-05-21 14:31:58 +0530

    fixes

--- apps/frontend/src/constants/constants.ts
+++ apps/frontend/src/constants/constants.ts
@@ -2,9 +2,7 @@
  * Defines frontend API constants and endpoint paths.
  */
 export const CONSTANTS = {
-  API_URL:
-    process.env.NEXT_PUBLIC_API_URL ??
-    "https://previgilant-dixie-disposingly.ngrok-free.dev",
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
   ACCESS_TOKEN: "access_token",
   AUTH_USER: "auth_user_data",
   REFRESH_TOKEN: "refresh_token",
